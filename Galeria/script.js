240ef30c61820ea525ef9d4208df450bf23a979d
¨jessica-maribel¨ <jessicaguncay45@gmail.com>
2020-05-11 21:42:30 -0500

    Arreglo de imagenes descargadas y almacenadas en una carpeta imagenes

---
+++ Galeria/script.js
@@ -0,0 +1,24 @@
+misFotos=[]
+muestra=0;
+window.onload = function() {
+
+    pantalla=document.getElementById("visor"); 
+    foto=document.getElementById("fotoSale"); 
+    fotoAnt=document.getElementById("fotoEntra"); 
+    nueva=document.getElementById("sss");
+}
+var aux=0;
+function randomico(){
+    //Cargar Imagenes 
+    misFotos=["<img src='imagenes/img1.jpg' alt='' >",
+    "<img src='imagenes/img2.jpg' alt='' >",
+    "<img src='imagenes/img3.jpg' alt='' >",
+    "<img src='imagenes/img4.jpg' alt='' >",
+    "<img src='imagenes/img5.jpg' alt='' >",
+    "<img src='imagenes/img6.jpg' alt='' >",
+    "<img src='imagenes/img7.jpg' alt='' >",
+    "<img src='imagenes/img8.jpg' alt='' >",
+    "<img src='imagenes/img9.jpg' alt='' >",
+    "<img src='imagenes/img10.jpg' alt='' >"]
+    
+  }
