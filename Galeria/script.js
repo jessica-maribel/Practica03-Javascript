9dd77e0fd8b75767b350908d086ee7db0e3ebaa2
¨jessica-maribel¨ <jessicaguncay45@gmail.com>
2020-05-11 21:43:43 -0500

    Se debe visualizar 5 imagenes y escoger de manera aleatoria las 5 imagenes

--- Galeria/script.js
+++ Galeria/script.js
@@ -20,5 +20,25 @@ function randomico(){
     "<img src='imagenes/img8.jpg' alt='' >",
     "<img src='imagenes/img9.jpg' alt='' >",
     "<img src='imagenes/img10.jpg' alt='' >"]
-    
+    for (var i=1; i<6; i++) {
+        //var aleatorio = Math.floor(Math.random()*11)+1;
+        var aleatorio = Math.ceil(Math.random()*11);
+        misFotos[i]="<img src ='imagenes/img"+aleatorio +".jpg' alt='' >";
+        var existe = false;
+        for (var i=0; i<6; i++){
+            if (misFotos [i] == aleatorio){
+                existe =true;
+                break;
+            }
+        }
+        if (!existe) {
+            misFotos[6]=aleatorio;
+            
+        }
+        //aux=aleatorio;
+        console.log('vectorqq: '+aleatorio)
+    }
+    console.log('vectorww: '+ misFotos[aleatorio])
+    foto.innerHTML=misFotos[1];
+    muestra=1;
   }
